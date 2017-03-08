d457728c7227dcd77ed6ae640ec06b2af3163eaa
yinm <yinm@pepabo.com>
2017-03-08 09:30:59 +0900

    chapter6 done

--- oneline_bbs/bbs.php
+++ oneline_bbs/bbs.php
@@ -73,43 +73,4 @@ try {
     $selectStatement = null;
     $dbh = null;
 
-?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title>ひとこと掲示板</title>
-</head>
-<body>
-    <h1>ひとこと掲示板</h1>
-
-    <form action="bbs.php" method="post">
-        <?php if (count($validationErrors)): ?>
-        <ul class="error_list">
-            <?php foreach ($validationErrors as $error): ?>
-                <li>
-                    <?php echo htmlspecialchars($error, ENT_QUOTES, 'UTF-8') ?>
-                </li>
-            <?php endforeach; ?>
-        </ul>
-        <?php endif; ?>
-        名前： <input type="text" name="name"><br>
-        ひとこと： <input type="text" name="comment" size="60"><br>
-        <input type="submit" name="submit" value="送信">
-    </form>
-
-
-    <?php if (count($posts) > 0): ?>
-    <ul>
-        <?php foreach ($posts as $post): ?>
-        <li>
-            <?php echo htmlspecialchars($post['name'], ENT_QUOTES, 'UTF-8'); ?>:
-            <?php echo htmlspecialchars($post['comment'], ENT_QUOTES, 'UTF-8'); ?>
-            - <?php echo htmlspecialchars($post['created_at'], ENT_QUOTES, 'UTF-8'); ?>
-        </li>
-        <?php endforeach; ?>
-    </ul>
-    <?php endif; ?>
-
-</body>
-</html>
+include 'views/bbs_view.php';
