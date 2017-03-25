c19415a9ea0ad9110bef05c35b3dc94a30920b39
yinm <yinm.cminor9@gmail.com>
2017-03-25 15:07:16 +0900

    7.2.24

---
+++ application/core/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+abstract class Controller
+{
+    protected $controller_name;
+    protected $action_name;
+    protected $application;
+    protected $request;
+    protected $response;
+    protected $session;
+    protected $db_manager;
+
+    public function __construct($application)
+    {
+        $this->controller_name = strtolower(substr(get_class($this), 0, -10));
+
+        $this->application = $application;
+        $this->request     = $application->getRequest();
+        $this->response    = $application->getResponse();
+        $this->session     = $application->getSessioon();
+        $this->db_manager  = $application->getDbManager();
+    }
+}
