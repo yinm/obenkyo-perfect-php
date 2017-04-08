072c1381395fe30c93c343f29ac5607eb7a0f624
yinm <yinm.cminor9@gmail.com>
2017-04-09 08:34:29 +0900

    8.1.6

---
+++ mini-blog.localhost/MiniBlogApplication.php
@@ -0,0 +1,25 @@
+<?php
+
+class MiniBlogApplication extends Application
+{
+    protected $login_action = array('account', 'signin');
+
+    public function getRootDir()
+    {
+        return dirname(__FILE__);
+    }
+
+    protected function registerRoutes()
+    {
+        return array();
+    }
+
+    protected function configure()
+    {
+        $this->db_manager->connect('master', array(
+            'dsn'      => 'mysql:dbname=mini_blog;host=localhost',
+            'user'     => 'root',
+            'password' => '',
+        ));
+    }
+}
