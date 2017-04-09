ecb96749e865c4dfe6347d8de6d0163bbbe6bf9d
yinm <yinm.cminor9@gmail.com>
2017-04-09 12:50:53 +0900

    8.3.1

---
+++ mini-blog.localhost/models/StatusRepository.php
@@ -0,0 +1,33 @@
+<?php
+
+class StatusRepository extends DbRepository
+{
+    public function insert($user_id, $body)
+    {
+        $now = new DateTime();
+
+        $sql = "
+            INSERT INTO status(user_id, body, created_at)
+            VALUES(:user_id, :body, :created_at)
+        ";
+
+        $stmt = $this->execute($sql, array(
+            ':user_id'    => $user_id,
+            ':body'       => $body,
+            ':cretead_at' => $now->format('Y-m-d H:i:s'),
+        ));
+    }
+
+    public function fetchAllPersonalArchiveByUserId($user_id)
+    {
+        $sql = "
+            SELECT a.*, u.user_name
+            FROM status a
+                LEFT JOIN user u ON a.user_id = u.id
+            WHERE u.id = :user_id
+            ORDER BY a.cretaed_at DESC;
+        ";
+
+        return $this->fetchAll($sql, array(':user_id' => $user_id));
+    }
+}
