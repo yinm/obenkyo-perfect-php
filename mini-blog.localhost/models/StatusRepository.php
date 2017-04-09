65b12e2a7710c677a9476e83b0310be5338dd7c4
yinm <yinm.cminor9@gmail.com>
2017-04-09 17:07:02 +0900

    8.3.1のSQLの間違いを修正

--- mini-blog.localhost/models/StatusRepository.php
+++ mini-blog.localhost/models/StatusRepository.php
@@ -24,10 +24,13 @@ class StatusRepository extends DbRepository
             SELECT a.*, u.user_name
             FROM status a
                 LEFT JOIN user u ON a.user_id = u.id
-            WHERE u.id = :user_id
+                LEFT JOIN following f ON f.following_id = a.user_id
+                  AND f.user_id = :user_id
+            WHERE f.user_id = :user_id OR u.id = :user_id
             ORDER BY a.created_at DESC;
         ";
 
         return $this->fetchAll($sql, array(':user_id' => $user_id));
     }
+
 }
