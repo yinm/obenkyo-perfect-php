25003edc028614c11d00aea20f8e1dffb2bdffb3
yinm <yinm.cminor9@gmail.com>
2017-04-09 13:53:48 +0900

    8.3.3

---
+++ mini-blog.localhost/views/status/status.php
@@ -0,0 +1,9 @@
+<div class="status">
+    <div class="status_content">
+        <?php echo $this->escape($status['user_name']); ?>
+        <?php echo $this->escape($status['body']); ?>
+    </div>
+    <div>
+        <?php echo $this->escape($status['created_at']); ?>
+    </div>
+</div>
