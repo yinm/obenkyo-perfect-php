e2911f275a65b1a611852e054ed9aa2a66a7ee6e
yinm <yinm.cminor9@gmail.com>
2017-04-10 23:08:04 +0900

    8.5.6

--- mini-blog.localhost/views/layout.php
+++ mini-blog.localhost/views/layout.php
@@ -5,9 +5,22 @@
   <title><?php if (isset($title)): echo $this->escape($title) . ' - '; endif; ?>Mini Blog</title>
 </head>
 <body>
-<div>
+<div id="header">
   <h1><a href="<?php echo $base_url; ?>/">Mini Blog</a></h1>
 </div>
+
+<div id="nav">
+  <p>
+    <?php if ($session->isAuthenticated()): ?>
+        <a href="<?php echo $base_url; ?>">ホーム</a>
+        <a href="<?php echo $base_url; ?>/account">アカウント</a>
+    <?php else: ?>
+        <a href="<?php echo $base_url; ?>/account/signin">ログイン</a>
+        <a href="<?php echo $base_url; ?>/account/signup">アカウント登録</a>
+    <?php endif; ?>
+  </p>
+</div>
+
 <div id="main">
   <?php echo $_content; ?>
 </div>
