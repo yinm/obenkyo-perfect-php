f0e88f6fc1bfa02eb33fdc1ed13cec57480ca100
yinm <yinm.cminor9@gmail.com>
2017-04-10 22:19:13 +0900

    8.4.3

--- mini-blog.localhost/controllers/StatusController.php
+++ mini-blog.localhost/controllers/StatusController.php
@@ -61,4 +61,21 @@ class StatusController extends Controller
             '_token'   => $this->generateCsrfToken('status/post'),
         ), 'index');
     }
+
+    public function userAction($params)
+    {
+        $user = $this->db_manager->get('User')
+            ->fetchByUserName($params['user_name']);
+        if (!$user) {
+            $this->forward404();
+        }
+
+        $statuses = $this->db_manager->get('Status')
+            ->fetchAllByUserId($user['id']);
+
+        return $this->render(array(
+            'user'     => $user,
+            'statuses' => $statuses,
+        ));
+    }
 }
---
+++ mini-blog.localhost/views/status/user.php
@@ -0,0 +1,9 @@
+<?php $this->setLayoutVar('title', $user['user_name']) ?>
+
+<h2><?php echo $this->escape($user['user_name']); ?></h2>
+
+<div id="statuses">
+  <?php foreach ($statuses as $status): ?>
+  <?php echo $this->render('status/status', array('status' => $status)); ?>
+  <?php endforeach; ?>
+</div>
