6e1f6015db270aa7e0f4ce357384b91fc1644969
yinm <yinm.cminor9@gmail.com>
2017-04-11 23:15:41 +0900

    8.6.4

--- mini-blog.localhost/controllers/AccountController.php
+++ mini-blog.localhost/controllers/AccountController.php
@@ -2,7 +2,7 @@
 
 class AccountController extends Controller
 {
-    protected $auth_actions = array('index', 'signout');
+    protected $auth_actions = array('index', 'signout', 'follow');
 
     public function signupAction()
     {
@@ -151,5 +151,38 @@ class AccountController extends Controller
         $this->session->setAuthenticated(false);
 
         return $this->redirect('/account/signin');
+    }
+
+    public function followAction()
+    {
+        if (!$this->request->isPost()) {
+            $this->forward404();
+        }
+
+        $following_name = $this->request->getPost('following_name');
+        if (!$following_name) {
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+        if (!$this->checkCsrfToken('account/follow', $token)) {
+            return $this->redirect('/user/' . $following_name);
+        }
+
+        $follow_user = $this->db_manager->get('User')
+            ->fetchByUserName($following_name);
+        if (!$follow_user) {
+            $this->forward404();
+        }
+
+        $user = $this->session->get('user');
+
+        $following_repository = $this->db_manager->get('Following');
+        if ($user['id'] !== $follow_user['id']
+            && !$following_repository->isFollowing($user['id'], $follow_user['id'])) {
+            $following_repository->insert($user['id'], $follow_user['id']);
+        }
+
+        return $this->redirect('/account');
     }
 }
