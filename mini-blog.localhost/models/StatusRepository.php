ba3bb31850af0c13d3c2bbc60f1e5fc43f0ab777
yinm <yinm.cminor9@gmail.com>
2017-04-11 23:34:32 +0900

    8.6.6

--- mini-blog.localhost/models/StatusRepository.php
+++ mini-blog.localhost/models/StatusRepository.php
@@ -23,11 +23,11 @@ class StatusRepository extends DbRepository
         $sql = "
             SELECT a.*, u.user_name
             FROM status a
-                LEFT JOIN user u ON a.user_id = u.id
-                LEFT JOIN following f ON f.following_id = a.user_id
-                  AND f.user_id = :user_id
+            LEFT JOIN user u ON a.user_id = u.id
+            LEFT JOIN following f ON f.following_id = a.user_id
+               AND f.user_id = :user_id
             WHERE f.user_id = :user_id OR u.id = :user_id
-            ORDER BY a.created_at DESC;
+            ORDER BY a.created_at DESC
         ";
 
         return $this->fetchAll($sql, array(':user_id' => $user_id));
