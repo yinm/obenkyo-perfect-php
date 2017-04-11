8f4e2615fd2e51c14510a0ccf496a63e0ce33e33
yinm <yinm.cminor9@gmail.com>
2017-04-11 22:45:48 +0900

    8.6.3

--- mini-blog.localhost/controllers/StatusController.php
+++ mini-blog.localhost/controllers/StatusController.php
@@ -75,9 +75,20 @@ class StatusController extends Controller
         $statuses = $this->db_manager->get('Status')
             ->fetchAllByUserId($user['id']);
 
+        $following = null;
+        if ($this->session->isAuthenticated()) {
+            $my = $this->session->get('user');
+            if ($my['id'] !== $user['id']) {
+                $following = $this->db_manager->get('Following')
+                    ->isFollowing($my['id'], $user['id']);
+            }
+        }
+
         return $this->render(array(
-            'user'     => $user,
-            'statuses' => $statuses,
+            'user'      => $user,
+            'statuses'  => $statuses,
+            'following' => $following,
+            '_token'    => $this->generateCsrfToken('account/follow'),
         ));
     }
 
