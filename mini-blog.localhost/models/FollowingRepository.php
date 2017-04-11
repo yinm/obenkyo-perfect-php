ac75ffdf53f1bc10c1291c1c126c175b5d94cd37
yinm <yinm.cminor9@gmail.com>
2017-04-11 22:06:25 +0900

    8.6.2

---
+++ mini-blog.localhost/models/FollowingRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+class FollowingRepository extends DbRepository
+{
+    public function insert($user_id, $following_id)
+    {
+        $sql = "INSERT INTO following VALUES(:user_id, :following)";
+
+        $stmt = $this->execute($sql, array(
+            ':user_id'      => $user_id,
+            ':following_id' => $following_id,
+        ));
+    }
+
+    public function isFollowing($user_id, $following_id)
+    {
+        $sql = "
+            SELECT COUNT(user_id) as count
+            FROM following
+            WHERE user_id = :user_id
+              AND following_id = :following_id
+        ";
+
+        $row = $this->fetch($sql, array(
+            ':user_id'      => $user_id,
+            ':following_id' => $following_id,
+        ));
+
+        if ($row['count'] === '0') {
+            return true;
+        }
+
+        return false;
+    }
+}
