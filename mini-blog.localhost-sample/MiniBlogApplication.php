53ad9e30aeb40ba9ca84012506ac0fdbd46d7489
yinm <yinm.cminor9@gmail.com>
2017-04-13 08:22:47 +0900

    8.2.6

--- mini-blog.localhost-sample/MiniBlogApplication.php
+++ mini-blog.localhost-sample/MiniBlogApplication.php
@@ -12,6 +12,10 @@ class MiniBlogApplication extends Application
     protected function registerRoutes()
     {
         return array(
+            '/account'
+                => array('controller' => 'account', 'action' => 'index'),
+            '/account/:action'
+                => array('controller' => 'account'),
         );
     }
 
---
+++ mini-blog.localhost-sample/views/account/signup.php
@@ -0,0 +1,28 @@
+<?php $this->setLayoutVar('title', 'アカウント登録'); ?>
+
+<h2>アカウント登録</h2>
+
+<form action="<?php echo $base_url ?>/account/register" method="post">
+    <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>">
+
+  <table>
+    <tbody>
+      <tr>
+        <th>ユーザID</th>
+        <td>
+          <input type="text" name="user_name" value="">
+        </td>
+      </tr>
+      <tr>
+        <th>パスワード</th>
+        <td>
+          <input type="password" name="password" value="">
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <p>
+    <input type="submit" value="登録">
+  </p>
+</form>
