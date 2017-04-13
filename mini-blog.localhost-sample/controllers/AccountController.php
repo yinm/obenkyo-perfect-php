fd3378aafe2de2becb9a0b209633ab1bce9467e0
yinm <yinm.cminor9@gmail.com>
2017-04-13 22:10:26 +0900

    8.2.8

--- mini-blog.localhost-sample/controllers/AccountController.php
+++ mini-blog.localhost-sample/controllers/AccountController.php
@@ -8,4 +8,51 @@ class AccountController extends Controller
             '_token' => $this->generateCsrfToken('account/signup')
         ));
     }
+
+    public function registerAction()
+    {
+        if (!$this->request->isPost()) {
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+        if (!$this->checkCsrfToken('account/signup', $token)) {
+            return $this->redirect('/account/signup');
+        }
+        $user_name = $this->request->getPost('uesr_name');
+        $password = $this->request->getPost('password');
+
+        $errors = array();
+
+        if (!strlen($user_name)) {
+            $errors[] = 'ユーザIDを入力してください';
+        } elseif (!preg_match('/^\w{3, 20}$/', $user_name)) {
+            $errors[] = 'ユーザIDは半角英数字およびアンダースコアを3 ~ 20文字以内で入力してください';
+        } elseif (!$this->db_manager->get('User')->isUniqueUserName($user_name)) {
+            $errors[] = 'ユーザIDは既に使用されています';
+        }
+
+        if (!strlen($password)) {
+            $errors[] = 'パスワードを入力してください';
+        } elseif (strlen($password) < 4 || strlen($password) > 30) {
+            $errors[] = 'パスワードは4 ~ 30文字以内で入力してください';
+        }
+
+        if (count($errors) === 0) {
+            $this->db_manager->get('User')->insert($user_name, $password);
+            $this->session->setAuthenticated(true);
+
+            $user = $this->db_manager->get('User')->fetchByUserName($user_name);
+            $this->session->set('user', $user);
+
+            return $this->redirect('/');
+        }
+
+        return $this->render(array(
+            'user_name' => $user_name,
+            'password'  => $password,
+            'errors'    => $errors,
+            '_token'    => $this->generateCsrfToken('account/signup'),
+        ), 'signup');
+    }
 }
