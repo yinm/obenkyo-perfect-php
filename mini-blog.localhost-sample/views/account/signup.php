55940efe0376570c30c2b2def2f617d58e97a7d6
yinm <yinm.cminor9@gmail.com>
2017-04-14 23:06:28 +0900

    8.3.3

--- mini-blog.localhost-sample/views/account/signup.php
+++ mini-blog.localhost-sample/views/account/signup.php
@@ -6,29 +6,13 @@
     <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>">
 
   <?php if (isset($errors) && count($errors) > 0): ?>
-      <ul class="error_list">
-        <?php foreach ($errors as $error): ?>
-            <li><?php echo $this->escape($error); ?></li>
-        <?php endforeach; ?>
-      </ul>
+      <?php echo $this->render('errors', array('errors' => $errors)); ?>
   <?php endif; ?>
 
-  <table>
-    <tbody>
-      <tr>
-        <th>ユーザID</th>
-        <td>
-          <input type="text" name="user_name" value="<?php echo $this->escape($user_name); ?>">
-        </td>
-      </tr>
-      <tr>
-        <th>パスワード</th>
-        <td>
-          <input type="password" name="password" value="<?php echo $this->escape($password); ?>">
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <?php echo $this->render('account/inputs', array(
+      'uesr_name' => $user_name,
+      'password'  => $password,
+  )); ?>
 
   <p>
     <input type="submit" value="登録">
--- mini-blog.localhost-sample/views/status/index.php
+++ mini-blog.localhost-sample/views/status/index.php
@@ -5,7 +5,9 @@
 <form action="<?php echo $base_url; ?>/status/post" method="post">
   <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>">
 
-  <?php //todo エラー処理 ?>
+  <?php if (isset($errors) && count($errors) > 0): ?>
+      <?php echo $this->render('errors', array('errors' => $errors)); ?>
+  <?php endif; ?>
 
   <textarea name="body" rows="2" cols="60"><?php echo $this->escape($body); ?></textarea>
 
@@ -16,14 +18,6 @@
 
 <div id="statuses">
   <?php foreach ($statuses as $status): ?>
-      <div class="status">
-        <div class="status_content">
-          <?php echo $this->escape($status['user_name']); ?>
-          <?php echo $this->escape($status['body']); ?>
-        </div>
-        <div>
-          <?php echo $this->escape($status['created_at']); ?>
-        </div>
-      </div>
+      <?php echo $this->render('status/status', array('status' => $status)); ?>
   <?php endforeach; ?>
 </div>
