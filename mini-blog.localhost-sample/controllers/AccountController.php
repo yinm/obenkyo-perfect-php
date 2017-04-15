a0e3d7a1f61420a062b5971fd8ba55dde1d25c8b
yinm <yinm.cminor9@gmail.com>
2017-04-15 11:21:58 +0900

    8.5.4

--- mini-blog.localhost-sample/controllers/AccountController.php
+++ mini-blog.localhost-sample/controllers/AccountController.php
@@ -85,4 +85,54 @@ class AccountController extends Controller
             '_token'    => $this->generateCsrfToken('account/signin'),
         ));
     }
+
+    public function authenticateAction()
+    {
+        if ($this->session->isAuthenticated()) {
+            return $this->redirect('/account');
+        }
+
+        if (!$this->request->isPost()) {
+            $this->forward404();
+        }
+
+        $token = $this->request->getPost('_token');
+        if (!$this->checkCsrfToken('account/signin', $token)) {
+            return $this->redirect('/account/signin');
+        }
+
+        $user_name = $this->request->getPost('user_name');
+        $password  = $this->request->getPost('password');
+
+        $errors = array();
+
+        if (!strlen($user_name)) {
+            $errors[] = 'ユーザIDを入力してください';
+        }
+
+        if (!strlen($password)) {
+            $errors[] = 'パスワードを入力してください';
+        }
+
+        if (count($errors) === 0) {
+            $user_repository = $this->db_manager->get('User');
+            $user = $user_repository->fetchByUserName($user_name);
+
+            if ($user || ($user['password'] !== $user_repository->hashPassword($password))) {
+                $errors[] = 'ユーザIDかパスワードが不正です';
+            } else {
+                $this->session->setAuthenticated(true);
+                $this->session->set('user', $user);
+
+                return $this->redirect('/');
+            }
+        }
+
+        return $this->render(array(
+            'user_name' => $user_name,
+            'password'  => $password,
+            'errors'    => $errors,
+            '_token'    => $this->generateCsrfToken('account/signin'),
+        ), 'signin');
+    }
 }
