4d4a554d8d473d48be7075c4813c6670d423b71e
yinm <yinm.cminor9@gmail.com>
2017-04-15 11:32:06 +0900

    8.5.6

--- mini-blog.localhost-sample/views/layout.php
+++ mini-blog.localhost-sample/views/layout.php
@@ -10,6 +10,18 @@
   <h1><a href="<?php echo $base_url; ?>">Mini Blog</a></h1>
 </div>
 
+<div id="nav">
+  <p>
+    <?php if ($session->isAuthenticated()): ?>
+        <a href="<?php echo $base_url; ?>/">ホーム</a>
+        <a href="<?php echo $base_url; ?>/account">アカウント</a>
+    <?php else: ?>
+        <a href="<?php echo $base_url; ?>/account/signin">ログイン</a>
+        <a href="<?php echo $base_url; ?>/account/signup">アカウント登録</a>
+    <?php endif; ?>
+  </p>
+</div>
+
 <div id="main">
   <?php echo $_content; ?>
 </div>
