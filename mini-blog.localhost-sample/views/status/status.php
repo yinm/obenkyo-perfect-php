bcaae89149df5afa2424a45688d5cc6ba7ce98ec
yinm <yinm.cminor9@gmail.com>
2017-04-15 10:47:44 +0900

    bug fix

--- mini-blog.localhost-sample/views/status/status.php
+++ mini-blog.localhost-sample/views/status/status.php
@@ -8,7 +8,7 @@
     <div>
       <a href="<?php echo $base_url;
       ?>/user/<?php echo $this->escape($status['user_name']);
-      ?>/status/<?php echo $this->escape($status['created_at']); ?>">
+      ?>/status/<?php echo $this->escape($status['id']); ?>">
           <?php echo $this->escape($status['created_at']); ?>
       </a>
     </div>
