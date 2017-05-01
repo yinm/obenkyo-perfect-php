6ad581a39050c6efbdf99a5dede181db49c5f8c9
yinm <yinm@pepabo.com>
2017-05-01 09:40:55 +0900

    7.2.17 (DbMaterの最低限の実装)

---
+++ second/application/core/DbManager.php
@@ -0,0 +1,43 @@
+<?php
+
+class DbManager
+{
+    protected $connections = array();
+
+    /**
+     * @param string $name
+     * @param array $params
+     */
+    public function connect($name, $params)
+    {
+        $params = array_merge(array(
+            'dsn'      => null,
+            'user'     => '',
+            'password' => '',
+            'options'  => array(),
+        ), $params);
+
+        $con = new PDO(
+            $params['dsn'],
+            $params['user'],
+            $params['password'],
+            $params['options']
+        );
+
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $this->connections[$name] = $con;
+    }
+
+    /**
+     * @param string|null $name
+     * @return PDO
+     */
+    public function getConnection($name = null)
+    {
+        if (is_null($name)) {
+            return current($this->connections);
+        }
+
+        return $this->connections[$name];
+    }
+}
