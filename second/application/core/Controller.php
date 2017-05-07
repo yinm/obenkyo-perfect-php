254f48f055f6a3e8c06c4898200ccabf93ed9276
yinm <yinm@pepabo.com>
2017-05-08 08:58:50 +0900

    7.2.24

---
+++ second/application/core/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+abstract class Controller
+{
+    protected $controllerName;
+    protected $actionName;
+    protected $application;
+    protected $request;
+    protected $response;
+    protected $session;
+    protected $dbManager;
+
+    public function __construct($application)
+    {
+        $this->controllerName = strtolower(substr(get_class($this), 0, -10));
+
+        $this->application = $application;
+        $this->request     = $application->getRequest();
+        $this->response    = $application->getResponse();
+        $this->session     = $application->getSession();
+        $this->dbManager   = $application->getDbManager();
+    }
+}
