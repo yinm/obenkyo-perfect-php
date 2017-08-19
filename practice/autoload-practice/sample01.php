29e74f4312e050b7067398da0f55d3b3562a3b7e
yinm <yinm.cminor9@gmail.com>
2017-08-19 23:13:44 +0900

    try autoload

---
+++ practice/autoload-practice/sample01.php
@@ -0,0 +1,7 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$shinjuku = new \myapp\japan\tokyo\Shinjuku();
+
+echo $shinjuku->hello();
---
+++ practice/autoload-practice/src/japan/tokyo/shinjuku.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace myapp\japan\tokyo;
+
+class Shinjuku
+{
+    public function hello()
+    {
+        echo 'Hello Shinjuku!';
+    }
+}
